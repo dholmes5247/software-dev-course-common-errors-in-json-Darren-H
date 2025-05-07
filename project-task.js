5e1d9a8e79521786be438f72b6e97a762eecee52
Darren-Holmes <dholmes5247@hotmail.com>
2025-05-07 12:21:35 -0500

    New commit to capture answered questions

--- project-task.js
+++ project-task.js
@@ -38,34 +38,24 @@ ACTIONS TAKEN:
 }
 
   
-
-
-// ============================================
-// ✅ Requirements
-// ============================================
-
-/*
-- Use a JSON validator or linter (e.g., https://jsonlint.com/) to test your final version
-- Keep track of each fix:
-  • What was wrong?
-  • Why is it a problem in JSON?
-  • What did you change to fix it?
-*/
-
-
-// ============================================
-// 🤔 Follow-Up Questions
-// ============================================
-
 /*
 💬 Reflect and answer the following:
 
 1️⃣ What tools or techniques did you use to identify the errors?
+I started with eyeballing it.  I looked for mismatches and missing or extra commas, double quotes etc.  
+The file came over in JS so I didn't trust VS and the terminal to display correctly with that.  
 
 2️⃣ How did you confirm that your corrected JSON file was valid?
+Once I had made all the changes that I could see I copied and pasted the info into the website JSON Lint.
+It came back showing it was valid JSON
 
 3️⃣ Which errors were the most difficult to spot? Why?
+The extra commas at the end of an array.  When everything is on one line it is pretty easy to spot but when 
+they are starting a new line with each piece of info it can be problematic. 
 
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
    (e.g., syntax highlighting, linters, writing JSON by example)
+   Taking the info from the JS file and putting it into a JSON file within VS would give me real time feedback that 
+   I could trust, hopefully eliminating some errors.  I will make use of JSON Lint and Beautify, they both seem straightforward
+   and easy to use.  
 */
